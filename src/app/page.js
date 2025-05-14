09d93223b64bac527f0e37080be2b98786b93627
ariellenamelist <arielle.namelist@gmail.com>
2025-05-14 23:00:06 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -24,33 +24,9 @@ import Footer from "./components/footer/footer";
 export default function Home() {
   return (
     <div>
-    <!-- Meta Tags -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
-    <meta name="googlebot" content="index,follow,max-snippet:-1,max-image-preview:large,max-video-preview:-1">
-    <meta name="bingbot" content="index,follow,max-snippet:-1,max-image-preview:large,max-video-preview:-1">
-    <title>Welcome to gold365</title>
-    <meta name="title" content="Welcome to gold365"/>
-    <meta name="description" content="Gold365 is the world's largest gaming website. More than 1500+ sports games available daily on Gold365.com website. Cricket - Football - Tennis - Horse Racing - Rummy - Poker - Teenpatti - Ludo. Instant deposit and withdrawal. Best odds, 24/7 customer support and referral bonuses. Sign up for a Gold365 account now."/>
-    <meta name="keywords" content="welcome to gold365, gold365, gold365 login, gold365 login ID, gold365 ID, gold365 login ID and password, gold365 download apk, gold365 green, gold365 registration, gold365.green login link"/>
-    <meta name="author" content="Gold365"/>
-    <meta name="publisher" content="Gold365">
-    <meta http-equiv="Content-Language" content="en-IN">
-    <meta name="reply-to" content="support@gold365.org.in"/>
-    <meta name="copyright" content="https://www.gold365.org.in"/>
-    <link rel="canonical" href="https://www.gold365.org.in"/>
-    <link rel="alternate" type="application/rss+xml" href="https://www.gold365.org.in/sitemap.xml" />
-    <link rel="alternate" type="application/rss+xml" href="https://www.gold365.org.in/images_sitemap.xml" />
-
-    <!-- Open Graph meta tags for Gold365 -->
-    <meta property="og:url" content="https://www.gold365.org.in/">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="Welcome to Gold365">
-    <meta property="og:description" content="Gold365 is the world's largest gaming website. More than 1500+ sports games available daily on Gold365.com website. Cricket - Football - Tennis - Horse Racing - Rummy - Poker - Teenpatti - Ludo. Instant deposit and withdrawal. Best odds, 24/7 customer support and referral bonuses. Sign up for a Gold365 account now.">
-    <meta property="og:image" content="https://www.gold365.org.in/_next/image?url=%2Fimages%2Flogo.png&w=384&q=75">
-    <meta property="og:logo" content="https://www.gold365.org.in/_next/image?url=%2Fimages%2Flogo.png&w=384&q=75">
+    <title>Gold365 Win</title>
+    <meta name="title" content="Gold365 Win" />
+    <meta name="description" content="Gaming platforms like CricAdda are the most popular in India for betting, live casino, and fantasy games. There are over 5000+ sports games available every day on CricAdda.com. Log in to your CricAdda account now." />
    
     <div className={styles.pages}>
       <main className={styles.mains}>
