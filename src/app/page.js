0d7fc0284911d4da18c0e77272f4860bcd1e627e
ariellenamelist <arielle.namelist@gmail.com>
2025-05-15 15:37:57 +0530

    new v2 page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-//import Image from "next/image";
 import styles from "./page.module.css"
 import Login from "./components/login/login";
 import Widthdrawal from "./components/widthdrawal/widthdrawal";
@@ -25,9 +24,39 @@ export default function Home() {
   return (
     <div>
     <title>Welcome to gold365</title>
-    <meta name="title" content="Welcome to gold365" />
-    <meta name="description" content="Gold365 is the world's largest gaming website. More than 1500+ sports games available daily on Gold365.com website. Cricket - Football - Tennis - Horse Racing - Rummy - Poker - Teenpatti - Ludo. Instant deposit and withdrawal. Best odds, 24/7 customer support and referral bonuses. Sign up for a Gold365 account now." />
-    <meta name="googlebot" content="index,follow,max-snippet:-1,max-image-preview:large,max-video-preview:-1">
+    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
+    <meta name="googlebot" content="index,follow,max-snippet:-1,max-image-preview:large,max-video-preview:-1"/>
+    <meta name="bingbot" content="index,follow,max-snippet:-1,max-image-preview:large,max-video-preview:-1"/>
+    
+    <meta name="title" content="Welcome to gold365"/>
+    <meta name="description" content="Gold365 is the world's largest gaming website. More than 1500+ sports games available daily on Gold365.com website. Cricket - Football - Tennis - Horse Racing - Rummy - Poker - Teenpatti - Ludo. Instant deposit and withdrawal. Best odds, 24/7 customer support and referral bonuses. Sign up for a Gold365 account now."/>
+    <meta name="keywords" content="welcome to gold365, gold365, gold365 login, gold365 login ID, gold365 ID, gold365 login ID and password, gold365 download apk, gold365 green, gold365 registration, gold365.green login link"/>
+    <meta name="author" content="Gold365"/>
+    <meta name="publisher" content="Gold365"/>
+    <meta httpEquiv="Content-Language" content="en-IN"/>
+    <meta name="reply-to" content="support@gold365.org.in"/>
+    <meta name="copyright" content="https://www.gold365.org.in"/>
+    <link rel="canonical" href="https://www.gold365.org.in"/>
+    <link rel="alternate" type="application/rss+xml" href="https://www.gold365.org.in/sitemap.xml" />
+    <link rel="alternate" type="application/rss+xml" href="https://www.gold365.org.in/images_sitemap.xml" />
+
+    {/*<!-- Open Graph meta tags for Gold365 -->*/}
+    <meta property="og:url" content="https://www.gold365.org.in/"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="Welcome to Gold365"/>
+    <meta property="og:description" content="Gold365 is the world's largest gaming website. More than 1500+ sports games available daily on Gold365.com website. Cricket - Football - Tennis - Horse Racing - Rummy - Poker - Teenpatti - Ludo. Instant deposit and withdrawal. Best odds, 24/7 customer support and referral bonuses. Sign up for a Gold365 account now."/>
+    <meta property="og:image" content="https://www.gold365.org.in/_next/image?url=%2Fimages%2Flogo.png&w=384&q=75"/>
+    <meta property="og:logo" content="https://www.gold365.org.in/_next/image?url=%2Fimages%2Flogo.png&w=384&q=75"/>
+
+    {/*<!-- Twitter Card meta tags for Gold365 -->*/}
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:domain" content="Gold365"/>
+    <meta name="twitter:url" content="https://www.gold365.org.in/"/>
+    <meta name="twitter:title" content="Welcome to Gold365"/>
+    <meta name="twitter:description" content="Gold365 is the world's largest gaming website. More than 1500+ sports games available daily on Gold365.com website. Cricket - Football - Tennis - Horse Racing - Rummy - Poker - Teenpatti - Ludo. Instant deposit and withdrawal. Best odds, 24/7 customer support and referral bonuses. Sign up for a Gold365 account now."/>
+    <meta name="twitter:image" content="https://www.gold365.org.in/_next/image?url=%2Fimages%2Flogo.png&w=384&q=75"/>
+    
+
     <div className={styles.pages}>
       <main className={styles.mains}>
 
